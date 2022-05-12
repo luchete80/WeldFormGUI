64a30fa9f115e6e4f1c21cc356112243c78d883b
luciano INTI Win <luciano.buglioni@gmail.com>
2022-05-12 12:41:57 -0300

    workign

--- src/editor.cpp
+++ src/editor.cpp
@@ -687,15 +687,15 @@ void Editor::RenderPhase(){
   //drawGui();
 
 
-  ////// IMGUI Rendering
-  // ImGui::Render();
-  // int display_w, display_h;
-  // glfwGetFramebufferSize(window, &display_w, &display_h);
-  // glViewport(0, 0, display_w, display_h);
-  // ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-  // // // // glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-  // // // // glClear(GL_COLOR_BUFFER_BIT);
-  // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+  //// IMGUI Rendering
+  ImGui::Render();
+  int display_w, display_h;
+  glfwGetFramebufferSize(window, &display_w, &display_h);
+  glViewport(0, 0, display_w, display_h);
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+  // // // glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+  // // // glClear(GL_COLOR_BUFFER_BIT);
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
 void Editor::Run(){
@@ -713,7 +713,7 @@ void Editor::Run(){
       
       PickingPhase();
       RenderPhase();
-      RenderBeams();
+      //RenderBeams();
 
       glEnable(GL_BLEND);
       glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
