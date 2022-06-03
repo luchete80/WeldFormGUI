9577dc8ed16ad920a2e955ca734611fedf2f17e2
luciano nb <luciano.buglioni@gmail.com>
2022-06-03 16:12:39 -0300

    updated shaders

--- data/picking.fs
+++ data/picking.fs
@@ -1,11 +1,12 @@
 #version 330                                                                        
                                                                                                                                                                                                                                                         
-uniform uint gDrawIndex;                                                            
-uniform uint gObjectIndex;                                                          
-                                                                                    
-out vec3 FragColor;
+uniform vec4 gPickingColor;
+                                                                             
+out vec4 FragColor;
 
 void main()                                                                         
 {                                                                                   
-   FragColor = vec3(float(gObjectIndex), float(gDrawIndex),float(gl_PrimitiveID + 1));                 
+   //FragColor = vec3(float(gObjectIndex), float(gDrawIndex),float(gl_PrimitiveID + 1));      
+  //FragColor = vec3(gObjectIndex/255., 255.0,255.0);        
+  FragColor =  gPickingColor; 
 }
