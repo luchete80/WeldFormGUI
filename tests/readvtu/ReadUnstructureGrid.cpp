7ba1ab2debd41aa5680d5cc018880eb051e4235c
INTI-Ubuntu <luciano.buglioni@gmail.com>
2024-08-30 11:38:54 -0300

    added things

---
+++ tests/readvtu/ReadUnstructureGrid.cpp
@@ -0,0 +1,63 @@
+#include <vtkActor.h>
+#include <vtkDataSetMapper.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkXMLUnstructuredGridReader.h>
+
+int main(int argc, char* argv[])
+{
+  // Parse command line arguments.
+  if (argc < 2)
+  {
+    std::cerr << "Usage: " << argv[0] << " Filename(.vtu) e.g. tetra.vtu"
+              << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::string filename = argv[1];
+
+  // Read all the data from the file.
+  vtkNew<vtkXMLUnstructuredGridReader> reader;
+  reader->SetFileName(filename.c_str());
+  reader->Update();
+
+  vtkNew<vtkNamedColors> colors;
+
+  // Create a mapper and actor.
+  vtkNew<vtkDataSetMapper> mapper;
+  mapper->SetInputConnection(reader->GetOutputPort());
+  mapper->ScalarVisibilityOff();
+
+  vtkNew<vtkActor> actor;
+  actor->SetMapper(mapper);
+  actor->GetProperty()->EdgeVisibilityOn();
+  actor->GetProperty()->SetLineWidth(2.0);
+  actor->GetProperty()->SetColor(colors->GetColor3d("MistyRose").GetData());
+
+  vtkNew<vtkProperty> backFace;
+  backFace->SetColor(colors->GetColor3d("Tomato").GetData());
+  actor->SetBackfaceProperty(backFace);
+
+  // Create a renderer, render window, and interactor.
+  vtkNew<vtkRenderer> renderer;
+  vtkNew<vtkRenderWindow> renderWindow;
+  renderWindow->AddRenderer(renderer);
+  vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+
+  // Add the actor to the scene.
+  renderer->AddActor(actor);
+  renderer->SetBackground(colors->GetColor3d("Wheat").GetData());
+
+  // Render and interact.
+  renderWindow->SetSize(640, 480);
+
+  renderWindow->Render();
+  renderWindowInteractor->Start();
+
+  return EXIT_SUCCESS;
+}
