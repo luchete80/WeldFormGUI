bdffcd59c2e91eba94b5a939d4d4d63d5f38299f
luciano nb <luciano.buglioni@gmail.com>
2024-11-25 16:45:32 -0300

    addedthings

---
+++ scripts/copy.bat
@@ -0,0 +1,6 @@
+copy src\model\model.dll src
+copy src\python\model.py src
+copy src\python\model.py src
+copy src\App\app.dll src
+copy src\graphicmesh\graphicmesh_.dll src
+copy src\python\_dnlPython.pyd src
