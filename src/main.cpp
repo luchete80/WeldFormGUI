14ac7b4f92e2f5b5b11c12ae460a383ad928281a
luciano_nb <luciano.buglioni@gmail.com>
2026-04-20 17:55:34 -0300

    adding things to avoi minmax

--- src/main.cpp
+++ src/main.cpp
@@ -77,6 +77,11 @@
 #include <vtkFileOutputWindow.h>
 #include <vtkOutputWindow.h>
 
+//THIS IS TO AVOIDERROR WITH MINMAX
+#ifdef _WIN32 
+  #undef min
+  #undef max
+#endif
 
 // Simple helper function to load an image into a OpenGL texture with common settings
 bool LoadTextureFromMemory(const void* data, size_t data_size, GLuint* out_texture, int* out_width, int* out_height)
